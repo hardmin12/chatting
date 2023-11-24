1ad3a450a337ffa5251926a3c2ff06922e6ea6d3
hardmin12 <hardmin1222@gamil.com>
2023-11-24 16:35:58 +0900

    chatclientSample upload

---
+++ chatClientSample/src/main/java/com/solomonm/chatClientSample/ChatClientSampleApplication.java
@@ -0,0 +1,13 @@
+package com.solomonm.chatClientSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatClientSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatClientSampleApplication.class, args);
+	}
+
+}
---
+++ chatClientSample/src/test/java/com/solomonm/chatClientSample/ChatClientSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solomonm.chatClientSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatClientSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
