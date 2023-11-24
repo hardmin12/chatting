1ef806078de41319b30bde6515792ae15c096933
hardmin12 <hardmin1222@gamil.com>
2023-11-24 11:21:43 +0900

    Chatting upload

---
+++ chatServer/src/test/java/com/solomonm/chatServer/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solomonm.chatServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
